1d7a7aef7f6efdacd05adc6e36956e473a911521
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-04-12 15:01:39 -0400

    Add files via upload
    add_recordfunction using csv file. Check for the sign up and user request function  if the data get properly saved into the respective files

---
+++ src/add_record.c
@@ -0,0 +1,44 @@
+int add_record(struct books a,int table)
+{
+
+        char tablestring[255];
+
+        switch(table){
+                case 1: strcpy(tablestring, charfile1);
+                        break;
+                case 2: strcpy(tablestring, charfile2);
+                        break;
+                case 3: strcpy(tablestring, charfile3);
+                        break;
+                default: printf("No match");
+        }
+
+        fa = fopen(tablestring,"a");
+        if(fa == NULL)
+        {
+        printf("File is not opened\n");
+        exit(1);
+        }
+
+	switch(table)
+	{
+
+        case 1 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+                a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
+                table_num = table;
+                break;
+        case 2 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+                a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
+                table_num = table;
+                break;
+        case 3 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+                a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
+                table_num = table;
+                break;
+	}
+        gotoxy(20,16);
+
+        fclose(fa);
+        return table_num;;
+
+}
