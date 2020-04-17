a4b4a851caf30ee0adc3b23fa24b6e30bac7a0b0
AishaSR <60946488+AishaSR@users.noreply.github.com>
2020-04-17 02:16:06 -0500

    Resolved issue with double listing includes.

--- src/display_page.c
+++ src/display_page.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 //#include "layout.c"
 #include "database_lookup.c"
 //#include "pages.c"
