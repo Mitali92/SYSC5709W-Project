cada58c01d1286ea9acd045158507d829e2cd8f9
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-04-16 20:50:28 -0400

    Updated with date for book details

--- src/add_record.c
+++ src/add_record.c
@@ -1,8 +1,5 @@
 int add_record(struct books a,int table)
 {
-
-        char tablestring[255];
-
         switch(table){
                 case 1: strcpy(tablestring, charfile1);
                         break;
@@ -23,20 +20,24 @@ int add_record(struct books a,int table)
 	switch(table)
 	{
 
-        case 1 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
-                a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
+        case 1 : printf("replace with sign up struct params");
+
+               //fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+                //a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
+                //table_num = table;
+                break;
+        case 2 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%d-%d-%d\,\"%s\",\"%s\",\"%s\",\"%d-%d-%d\"\n",
+                a.book_title,a.book_id,a.isbn_no,a.quantity,a.author_name,a.date_of_pub,a.entry_date,a.category,a.language);
                 table_num = table;
                 break;
-        case 2 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
-                a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
-                table_num = table;
+        case 3 :
+                printf("replace with user requests struct params");
+               // fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+               // a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
+                //table_num = table;
                 break;
-        case 3 : fprintf(fa,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
-                a.book_id,a.book_title,a.author_name,a.quantity,a.isbn_no,a.category,a.language);
-                table_num = table;
-                break;
-	}
-        gotoxy(20,16);
+        }
+        gotoxy(20,20);
 
         fclose(fa);
         return table_num;;
