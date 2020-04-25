32a827df8af22a09b25b43ba5eec1b2980b643c8
Mitali92 <mitalipatel3@cmail.carleton.ca>
2020-04-24 20:47:15 -0400

    updated modify_book() and modify_record()

--- src/modify_book.c
+++ src/modify_book.c
@@ -1,15 +1,62 @@
-#include <stdio.h>
-#include <errno.h>
 #include "layout.h"
+#include "manager_menu.h"
+#include "modify_record.h"
+#include "database_lookup.h"
+
 
 int edit_field();
-int int_check(char input[]);
+int modify_record(char search_term[], int table,int search_field);
+int data_valid(char search_term[],int search_field);
 
 char bookid[20];
 int edited;
 int choice;
 char isbn[20];
-char buff[255];
+
+int edit_field()
+{
+        int search_field;
+        gotoxy(40,11);printf("Choose the field to edit : \n");
+        gotoxy(40,13);printf("[1]. BOOK TITLE ");
+        gotoxy(40,15);printf("[2]. ISBN NO.");
+        gotoxy(40,17);printf("[3]. AUTHOR NAME");
+        gotoxy(40,19);printf("[4]. QUANTITY");
+        gotoxy(40,21);printf("[5]. CATEGORY");
+        gotoxy(40,23);printf("[6]. LANGUAGE");
+        gotoxy(40,25);printf("[7]. DATE OF PUB");
+        gotoxy(40,27);printf("[8]. Status");
+        gotoxy(40,29);printf("Select the filter : ");
+        fflush(stdout);
+        gotoxy(60,29);scanf("%d",&search_field);
+
+        return search_field;
+}
+
+int data_valid(char search_term[],int search_field){
+
+            char buff[255];
+            int isFound = 0;
+            FILE *returnFile = lookup(search_term,1,search_field);
+            while (fgets(buff, 1024, returnFile))
+            {
+                char *field = strtok(buff, "\",\"");
+                if(strcmp(field,search_term)==0)
+                {
+                    isFound = 1;
+                    break;
+                }
+                field = strtok(NULL, "\",\"");
+                field = strtok(NULL, "\",\"");
+
+                if(strcmp(field,search_term)==0)
+                {
+                    isFound = 1;
+                    break;
+                }
+            }
+
+        return isFound;
+}
 
 int modify_book()
 {
@@ -17,116 +64,86 @@ int modify_book()
         header_layout("MODIFY BOOK");
 
         gotoxy(40,11);printf("Choose the filter to search the book : \n");
-        gotoxy(40,14);printf("1. Book ID \n");
-        gotoxy(40,15);printf("2. ISBN Number \n");
-        gotoxy(40,17);printf("Select the filter : ");
+        gotoxy(40,14);printf("[1]. Book ID \n");
+        gotoxy(40,16);printf("[2]. ISBN Number \n");
+        gotoxy(40,18);printf("Select the filter : ");
         fflush(stdout);
     repeat:
-        gotoxy(60,17);scanf("%d",&choice);
+        gotoxy(60,18);scanf("%d",&choice);
         getchar();
 
         if(choice == 1)
         {
-            gotoxy(40,19);printf("Enter Book ID :");
+            gotoxy(40,20);printf("Enter Book ID :");
             fflush(stdout);
-            gotoxy(60,19);scanf("%s",bookid);
+        repeat_bookid:
+            gotoxy(60,20);scanf("%s",bookid);
             getchar();
-            display_table(lookup(bookid,1,1));
 
-            int isFound = 0;
-
-            FILE *returnFile = lookup(bookid,1,1);
-
-            while (fgets(buff, 1024, returnFile))
-            {
-                char *field = strtok(buff, "\",\"");
-                if(strcmp(field,bookid)==0)
-                {
-                    isFound = 1;
-                    break;
-                }
-                field = strtok(NULL, "\",\"");
-                field = strtok(NULL, "\",\"");
-
-                if(strcmp(field,bookid)==0)
-                {
-                    isFound = 1;
-                    break;
-                }
-            }
-
-        if(isFound==1)
+        if(data_valid(bookid,1)==1)
         {
+            gotoxy(40,11);printf("                                                                           ");
+            gotoxy(40,14);printf("                                                                           ");
+            gotoxy(40,16);printf("                                                                           ");
+            gotoxy(60,18);printf("                                                                           ");
+            gotoxy(40,18);printf("                                                                           ");
+            gotoxy(60,20);printf("                                                                           ");
+            gotoxy(40,20);printf("                                                                           ");
+
             edited = edit_field();
-
-            char *comp_result;
-            char editedValue[255];
-
-            switch (edited)
-            {
-                case 1: gotoxy(40,29);printf("Enter Title");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-                case 2: gotoxy(40,29);printf("Enter ISBN No.");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-                case 3: gotoxy(40,29);printf("Enter Author Name");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-                case 4: gotoxy(40,29);printf("Enter Quantity");
-                        fflush(stdout);
-                    quantity:
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        getchar();
-                        if(int_check(editedValue)!=0) {
-                            gotoxy(40,31);printf("Enter valid quantity");
-                            getchar();
-                            gotoxy(60,29);printf("                                                                           ");
-                            gotoxy(40,31);printf("                                                                           ");
-                            goto quantity;
-                        }
-                        break;
-                case 5: gotoxy(40,29);printf("Enter Category");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-                case 6: gotoxy(40,29);printf("Enter Language");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-                case 7: gotoxy(40,29);printf("Enter Date of Publication");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-                case 8: gotoxy(40,29);printf("Status");
-                        fflush(stdout);
-                        gotoxy(60,29);scanf("%s",editedValue);
-                        break;
-               default: gotoxy(40,29);printf("Invalid Filed. Please select proper field from the given choice ");
-                        getchar();
-                        gotoxy(60,29);printf("                                                                           ");
-                        gotoxy(40,32);printf("                                                                           ");
-                        //goto repeat;
+            if(modify_record(bookid,1,edited)==0){
+                getchar();
+                gotoxy(40,33);printf("Book details modified successfully");
+                gotoxy(40,35);printf("Press any key to cotinue to main menu....");
+                getchar();
+                manager_menu();
             }
-            //actual logic for modification is pending
         }
         else
         {
             gotoxy(40,21);printf("Data does not exist.Please try again");
             getchar();
-            gotoxy(60,17);printf("                                                                           ");
+            gotoxy(60,20);printf("                                                                           ");
             gotoxy(40,21);printf("                                                                           ");
-            gotoxy(60,19);printf("                                                                           ");
-            gotoxy(40,19);printf("                                                                           ");
-            goto repeat;
+            goto repeat_bookid;
         }
         }
         else if(choice == 2)
         {
+            gotoxy(40,19);printf("Enter ISBN No. :");
+            fflush(stdout);
+        repeat_isbn:
+            gotoxy(60,19);scanf("%s",isbn);
+            getchar();
 
+        if(data_valid(isbn,3)==1)
+        {
+            gotoxy(40,11);printf("                                                                           ");
+            gotoxy(40,14);printf("                                                                           ");
+            gotoxy(40,16);printf("                                                                           ");
+            gotoxy(60,18);printf("                                                                           ");
+            gotoxy(40,18);printf("                                                                           ");
+            gotoxy(60,20);printf("                                                                           ");
+            gotoxy(40,20);printf("                                                                           ");
+
+            edited = edit_field();
+            if(modify_record(isbn,1,edited)==0){
+                getchar();
+                gotoxy(40,33);printf("Book details modified successfully");
+                gotoxy(40,35);printf("Press any key to cotinue to main menu....");
+                getchar();
+                manager_menu();
+            };
+
+        }
+        else
+        {
+            gotoxy(40,21);printf("Data does not exist.Please try again");
+            getchar();
+            gotoxy(60,19);printf("                                                                           ");
+            gotoxy(40,21);printf("                                                                           ");
+            goto repeat_isbn;
+        }
         }
         else
         {
@@ -140,40 +157,3 @@ int modify_book()
     return 0;
 }
 
-int edit_field()
-{
-        int search_field;
-        gotoxy(40,17);printf("Choose the field to edit : \n");
-        gotoxy(40,19);printf("1. BOOK TITLE ");
-        gotoxy(40,20);printf("2. ISBN NO.");
-        gotoxy(40,21);printf("3. AUTHOR NAME");
-        gotoxy(40,22);printf("4. QUANTITY");
-        gotoxy(40,23);printf("5. CATEGORY");
-        gotoxy(40,24);printf("6. LANGUAGE");
-        gotoxy(40,25);printf("7. DATE OF PUB");
-        gotoxy(40,26);printf("8. Status");
-        gotoxy(40,27);printf("Select the filter : ");
-        fflush(stdout);
-        gotoxy(60,27);scanf("%d",&search_field);
-
-        return search_field;
-}
-
-int int_check(char input[])
-{
-    int size, x = 0;
-    size = strlen(input);
-
-    for(int i=0;i<size;i++)
-    {
-      if(input[i]=='0'||input[i]=='1'||input[i]=='2'||input[i]=='3'||input[i]=='5'||input[i]=='6'||input[i]=='7'||input[i]=='8'||input[i]=='9')
-      {
-           return 0;
-      }
-      else
-      {
-           continue;
-      }
-    }
-   return 1;
-}
