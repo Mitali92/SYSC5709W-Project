2343664c142598d478b366384528c647c2abc5b5
AishaSR <AishaSR@users.noreply.github.com>
2020-04-26 10:15:57 -0500

    Adds tests for all functions with inputs and outputs

--- src/test_lookup.c
+++ src/test_lookup.c
@@ -7,13 +7,18 @@
 #endif
 
 #include "../src/database_lookup.h"
+#include "add_books.h"
+#include "sign_up.h"
+#include "modify_book.h"
+#include "modify_record.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 
 
-
+////////////// AISHA's TESTS ***************
 START_TEST(test_long_search_term) {
 
   FILE *result;
@@ -64,9 +69,101 @@ START_TEST(test_all_good) {
   char check[6] = {0};
   fread(check, 1, 5, result);
   //Implies that the file returned is open with read permission and the pointer is at the top.
+  ck_assert_str_eq(check, "\"BOOK");
+  // This test will fail on purpose to demonstrate that a failure causes a message
+  // with the output and expected value
   ck_assert_str_ne(check, "\"BOOK");
 
 } END_TEST
+
+/************** MITALI's TESTS ****************/
+
+
+
+/************** GENERAL TESTS ****************/
+/* tests from database lookup */
+
+START_TEST(test_file_to_struct){
+    char *line;
+    strcpy(line,"\"Aisha\",\"AishaSR\",\"PassWe$d\",\"aisharobinson@cmail.carleton.ca\",\"2\"");
+    void *record = file_to_struct(line, 2);
+
+    struct user *test = record;
+
+    ck_assert_str_eq(test->name, "Aisha");
+    ck_assert_str_eq(test->user_name, "AishaSR");
+    ck_assert_str_eq(test->password, "PassWe$d");
+    ck_assert_str_eq(test->email, "aisharobinson@cmail.carleton.ca");
+    ck_assert_str_eq(test->number_of_books, "2");
+
+} END_TEST
+
+
+/* tests from add books */
+START_TEST(test_ISBN_validation){
+
+    ck_assert_int_eq(isbn_validation("12345",3), 1);
+    ck_assert_int_eq(isbn_validation("55555",3), 0);
+
+} END_TEST
+
+/* tests from sign up */
+START_TEST(test_char_check){
+
+    ck_assert_int_eq(char_check("1"), 0);
+    ck_assert_int_eq(char_check("t"), 1);
+
+} END_TEST
+
+START_TEST(test_pwd_check){
+
+    ck_assert_int_eq(char_check("spaced password "), 0);
+    ck_assert_int_eq(char_check("nospace"), 1);
+
+} END_TEST
+
+/* tests from modify_record */
+
+START_TEST(test_status_check){
+
+    ck_assert_int_eq(status_check("Available"), 0);
+    ck_assert_int_eq(status_check("UnAvailable"), 0);
+    ck_assert_int_eq(status_check("Used"), 1);
+
+} END_TEST
+
+START_TEST(test_int_check){
+
+    ck_assert_int_eq(int_check("1"), 0);
+    ck_assert_int_eq(int_check("45"), 0);
+    ck_assert_int_eq(int_check("Two"), 1);
+
+} END_TEST
+
+START_TEST(test_date_validation){
+
+    ck_assert_int_eq(date_validation("11-12-1991"), 0);
+    ck_assert_int_eq(date_validation("1112-1991"), 1);
+    ck_assert_int_eq(date_validation("11-15-1991"), 1);
+    ck_assert_int_eq(date_validation("11-15-191"), 1);
+    ck_assert_int_eq(date_validation("41-15-1991"), 1);
+    ck_assert_int_eq(date_validation("11"), 1);
+
+} END_TEST
+
+/* tests from modify_book */
+
+START_TEST(test_data_valid){
+
+    ck_assert_int_eq(data_valid("Dee",4), 1);
+    ck_assert_int_eq(data_valid("13",1), 1);
+    ck_assert_int_eq(data_valid("11-15-1991",8), 0);
+    ck_assert_int_eq(data_valid("horse", 2), 0);
+
+
+} END_TEST
+
+
 
 	////////////////
 Suite *test_suite(void) {
@@ -82,6 +179,15 @@ Suite *test_suite(void) {
   tcase_add_exit_test(tc_core, test_searchfield_ofb_2, 0);
   tcase_add_exit_test(tc_core, test_searchfield_ofb_3, 0);
   tcase_add_test(tc_core, test_all_good);
+  tcase_add_test(tc_core, test_ISBN_validation);
+  tcase_add_test(tc_core, test_char_check);
+  tcase_add_test(tc_core, test_pwd_check);
+  tcase_add_test(tc_core, test_status_check);
+  tcase_add_test(tc_core, test_int_check);
+  tcase_add_test(tc_core, test_date_validation);
+  tcase_add_test(tc_core, test_data_valid);
+  tcase_add_test(tc_core, test_file_to_struct);
+
   suite_add_tcase(s, tc_core);
 
   return s;
@@ -95,7 +201,7 @@ int main(void) {
   s = test_suite();
   runner = srunner_create(s);
   srunner_set_log (runner, "test.log");
-  srunner_run_all(runner, CK_NORMAL);
+  srunner_run_all(runner, CK_VERBOSE);
   no_failed = srunner_ntests_failed(runner);
   srunner_free(runner);
   return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
